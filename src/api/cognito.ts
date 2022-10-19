4c956adec22028db1f3f2f1934b41a6464f4adfe
leenawon <nawon5154@gmail.com>
2022-10-19 09:56:59 +0900

    Add: 로그인한 사용자 토큰 발급 API 작성

---
+++ src/api/cognito.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+import qs from "qs";
+
+// AWS Cognito 로그인 인증 API 호출
+export function GetNewTokens(code) {
+  return axios.post(
+    process.env.REACT_APP_ADMIN_COGNITO_HOST + "/oauth2/token",
+    qs.stringify({
+      client_id: process.env.REACT_APP_ADMIN_COGNITO_CLIENT_ID,
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: process.env.REACT_APP_ADMIN_COGNITO_REDIRECT_URI_LOGIN,
+    }),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: process.env.REACT_APP_ADMIN_COGNITO_AUTHORIZATION,
+      },
+    }
+  );
+}

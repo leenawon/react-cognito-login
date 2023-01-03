bfd88f4707af0773732471cf95c2bcc59a917269
leenawon <nawon5154@gmail.com>
2023-01-03 13:41:59 +0900

    Modify: 로그인 성공 후 메인 페이지에서 토큰 정보 확인

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,10 +1,14 @@
+// React
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 // 로그인 성공 후 메인 페이지 렌더링
 function MainPage() {
+  const { state } = useLocation();
   return (
     <div>
       <h1>MainPage</h1>
+      {/* <h3>{state.access_token}</h3> */}
     </div>
   );
 }
